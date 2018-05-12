63de5d307dc7c12446622334c6e8824d4867ea92
brandongatlin <brandongatlin1981@me.com>
2018-05-12 00:15:50 -0500

    cleaned up console logs

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,4 @@
 $( document ).ready( function () {
-  console.log( "app.js loaded" );
 
   $.get( "/", function ( req, res ) {
 
@@ -10,19 +9,13 @@ $( document ).ready( function () {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
-    console.log( 'submit clicked' );
-
     const shrimp_name = $( "#input-order" ).val().trim();
     const spiciness = $( "#spiciness" ).val();
-
-    console.log( "spiciness is:", spiciness );
 
     const order = {
       shrimp_name: shrimp_name,
       spiciness: spiciness
     }
-
-    console.log( 'order was:', order );
 
     // Send the POST request.
     $.ajax( "/order", {
@@ -30,7 +23,7 @@ $( document ).ready( function () {
       data: order
     } ).then(
       function () {
-        console.log( "ordered", order );
+        console.log( 'order was:', order );
         // Reload the page to get the updated list
         location.reload();
       }
@@ -45,8 +38,6 @@ $( document ).ready( function () {
   $( document ).on( "click", ".btn-order", function () {
     const item = $( this ).attr( "item" )
     const price = parseInt( $( this ).attr( "price" ) )
-
-    console.log( "initial total is:", total )
 
     const newOrder = {
       item: item,
